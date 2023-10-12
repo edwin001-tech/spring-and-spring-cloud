bc10d4e35567973ca97be555724ab42ddbbe2107
edwin001-tech <edwinshitakwa8@gmail.com>
2023-10-12 10:30:40 +0300

    initialize springboot application

---
+++ src/main/java/com/eduin/rest/webservices/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.eduin.rest.webservices;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//controller
+@RestController
+public class HelloWorldController {
+	
+	//GET 
+	//URI - /hello-world
+	//Method - 'Hello World'
+	
+	
+	@RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+	public String HelloWorld() {
+		return "Hello World";
+	}
+
+}
