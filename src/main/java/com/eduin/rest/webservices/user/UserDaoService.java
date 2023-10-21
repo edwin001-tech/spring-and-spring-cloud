fd6d1efac819d94667f66e6c8460504204eb511f
edwin001-tech <edwinshitakwa8@gmail.com>
2023-10-21 12:12:34 +0300

    get methods for user resource

--- src/main/java/com/eduin/rest/webservices/user/UserDaoService.java
+++ src/main/java/com/eduin/rest/webservices/user/UserDaoService.java
@@ -1,5 +1,48 @@
 package com.eduin.rest.webservices.user;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
 public class UserDaoService {
 
+	private static List<User> users = new ArrayList<>();
+	private static int usersCount = 3;
+	
+	static {
+		users.add(new User(1, "Edwin", new Date()));
+		users.add(new User(2, "Benson", new Date()));
+		users.add(new User(3, "Richard", new Date()));
+	}
+	
+	public List<User> findAll(){
+		return users;
+	}
+	
+	public User Save(User user) {
+		if(user.getId() == null) {
+			user.setId(++usersCount);
+		}
+		users.add(user);
+		return user;
+		
+	}
+	
+	public User findOne(int Id) {
+		for(User user:users) {
+			if(user.getId()==Id) {
+				return user;
+			}
+			
+		}
+		return null;
+		
+		
+	}
+		
+	
+	
 }
---
+++ src/main/java/com/eduin/rest/webservices/user/UserResource.java
@@ -0,0 +1,30 @@
+package com.eduin.rest.webservices.user;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserResource {
+	
+	@Autowired
+	private UserDaoService service;
+	
+	//GET Users
+	//retrieveAllUsers
+	
+	@GetMapping("/users")
+	public List<User> retrieveAllUsers(){
+		return service.findAll();
+	}
+	
+	//GET Users/{id}
+	@GetMapping("/users/{Id}")
+	public User retrieveUser(@PathVariable int Id){
+		return service.findOne(Id);
+	}
+
+}
