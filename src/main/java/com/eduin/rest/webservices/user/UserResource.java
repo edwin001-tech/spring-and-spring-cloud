1081be3710bfcc29b830e2ce7e6fd3448d09420b
edwin001-tech <edwinshitakwa8@gmail.com>
2023-10-26 00:02:12 +0300

    post method to return status code and location

--- src/main/java/com/eduin/rest/webservices/user/UserResource.java
+++ src/main/java/com/eduin/rest/webservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.eduin.rest.webservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -31,8 +34,21 @@ public class UserResource {
 	
 	//Post Users
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		User savedUser = service.Save(user);
+		
+		//CREATE
+		
+		URI location = ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("{/Id}")
+				.buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
+		
 	}
+	
+	
+	
+	
 
 }
